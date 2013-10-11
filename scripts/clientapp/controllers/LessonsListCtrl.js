0556abe311949a257c4881c7209200df42d23f32
Maximilian Alexander <maxalex@zinkpulse.com>
2013-10-11 16:43:26 -0400

    adding route link

--- scripts/clientapp/controllers/LessonsListCtrl.js
+++ scripts/clientapp/controllers/LessonsListCtrl.js
@@ -1,5 +1,14 @@
 ClientApp.Controllers.controller('LessonsListCtrl', ['$scope', 'chapters', function($scope, chapters){
 
-	$scope.chapters = chapters;
+	$scope.chapters = [];
+
+	_.each(chapters, function(chapter){
+
+		_.each(chapter.lessons, function(lesson){
+			lesson.routeLink = '#/chapters/' +  chapter.chapterId + '/lessons/' + lesson.lessonId;
+		})
+
+		$scope.chapters.push(chapter);
+	});
 
 }]);
